1308dc08cf1be78a2b09c8ef3ff194637f057cd8
Artem Bartov <ipbartov@gmail.com>
2024-04-17 18:26:44 +0500

    modified initial script

--- initial-setup.sh
+++ initial-setup.sh
@@ -2,15 +2,9 @@
 
 # Asking for username and groupname
 read -p "Enter the username: " USERNAME
-read -p "Enter the groupname (optional): " GROUPNAME
 
 # Creating user
 useradd -m "$USERNAME"
-
-# Creating group if needed
-if [ -n "$GROUPNAME" ]; then
-    usermod -aG "$GROUPNAME" "$USERNAME"
-fi
 
 # Creating user password
 passwd "$USERNAME"
@@ -20,11 +14,12 @@ usermod -aG sudo "$USERNAME"
 
 echo "User "$USERNAME" has been successfully added."
 
-# Adding ssh key
-mkdir -p /home/"$USERNAME"/.ssh/
-
-# Asking for public ssh key
-read -p "Enter public ssh key: " > /home/"$USERNAME"/.ssh/authorized_keys
+# Import ssh key from root
+rsync --archive --chown="$USERNAME":"$USERNAME" ~/.ssh /home/"$USERNAME"
 
 # Disabling root login for ssh
 sed -i 's/^PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+
+# Enabling firewall
+ufw allow OpenSSH
+ufw enable -y
