06b25309dbc6680d8179792395cdf56da3754609
Artem Bartov <ipbartov@gmail.com>
2024-04-17 18:49:32 +0500

    'somechanges'

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-# Asking for username and groupname
+# Asking for username
 read -p "Enter the username: " USERNAME
 
 # Creating user
@@ -11,15 +11,31 @@ passwd "$USERNAME"
 
 # Adding them to sudo group
 usermod -aG sudo "$USERNAME"
-
+  if [[ "$?" -eq 1 ]]; then
+    echo "Something went wrong. Please try again."
+    exit 1
+  fi
 echo "User "$USERNAME" has been successfully added."
 
 # Import ssh key from root
-rsync --archive --chown="$USERNAME":"$USERNAME" ~/.ssh /home/"$USERNAME"
+#rsync --archive --chown="$USERNAME":"$USERNAME" ~/.ssh /home/"$USERNAME"
+mkdir -p /home/"$USERNAME"/.ssh/
+cat ~/.ssh/authorized_keys > /home/"$USERNAME"/.ssh/authorized_keys
+chown -R "$USERNAME":"$USERNAME" /home/"$USERNAME"/.ssh/
 
 # Disabling root login for ssh
 sed -i 's/^PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 
 # Enabling firewall
+apt update -qq > /dev/null
+apt install ufw -yqq
 ufw allow OpenSSH
+  if [[ "$?" -eq 0 ]]; then
+    echo "Everything was Ok."
+  else
+    echo "Something went wrong, can't enable OpenSSH for firewall."
+    exit 1
+  fi
 ufw enable -y
+echo "Everything looks good. You can now login with user $USERNAME and your ssh key."
+exit 0
