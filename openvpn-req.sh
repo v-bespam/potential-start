f96223f5a515759e14e7a6b7fdc208a59a02fde0
Artem Bartov <ipbartov@gmail.com>
2024-04-30 22:42:34 +0500

    update openvpn-req

--- openvpn-req.sh
+++ openvpn-req.sh
@@ -9,29 +9,57 @@ fi
 # Setting Easy-RSA directory
 dir="/home/"$(whoami)"/easy-rsa"
 
-# Installing Easy-RSA and preparing directories
-sudo apt-get update -q
-sudo apt-get install openvpn easy-rsa -y
+# Function for installing OpenVPN if CA is configured on a separate server
+openvpn_inst ()
+{
+  # Installing Easy-RSA and preparing directories
+  sudo apt-get update -q
+  sudo apt-get install openvpn easy-rsa -y
   if [[ "$?" -eq 1 ]]; then
     echo "Can't install Easy-RSA. Please try again."
     exit 1
   fi
-mkdir "$dir"
-ln -s /usr/share/easy-rsa/* "$dir"
-chmod 700 "$dir"
-cd "$dir"
+}
 
-# Write some vars for Easy-RSA and init PKI
-echo "set_var EASYRSA_ALGO            \"ec\"" > "$dir"/vars
-echo "set_var EASYRSA_DIGEST          \"sha512\"" >> "$dir"/vars
+#Function for init PKI
+init-pki ()
+{
+  mkdir "$dir"
+  ln -s /usr/share/easy-rsa/* "$dir"
+  chmod 700 "$dir"
+  cd "$dir"
+  
+  # Write some vars for Easy-RSA and init PKI
+  echo "set_var EASYRSA_ALGO            \"ec\"" > "$dir"/vars
+  echo "set_var EASYRSA_DIGEST          \"sha512\"" >> "$dir"/vars
+  
+  "$dir"/easyrsa init-pki
+}
 
-"$dir"/easyrsa init-pki
+# Function for creating an OpenVPN Server Certificate Request and Private Key
+openvpn_req ()
+{
+  "$dir"/easyrsa gen-req server nopass
+  sudo cp "$dir"/pki/private/server.key /etc/openvpn/server/
+  
+  echo "Please sign the following OpenVPN server request in CA"
+  echo ""
+  cat "$dir"/pki/reqs/server.req
+}
 
-# Creating an OpenVPN Server Certificate Request and Private Key
-"$dir"/easyrsa gen-req server nopass
+echo "Please choose where you configured a CA server"
 
-# Copying Private Key to OpenVPN dir
-sudo cp "$dir"/pki/private/server.key /etc/openvpn/server/
-echo "Please sign the following OpenVPN server request in CA"
-echo ""
-cat "$dir"/pki/reqs/server.req
+case "$option" in
+  1) echo "On a separate server. Not here"
+    openvpn_inst
+    init-pki
+    openvpn_req
+  ;;
+  2) echo "On this server. Right here"
+    openvpn_inst
+    openvpn_req
+  ;;
+  *) echo "Bad option"
+    exit 1
+  ;;
+esac
